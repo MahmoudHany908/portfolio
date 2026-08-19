132ae40f12da8fada1b7eaef7390e995971af0da
Mahmoud Hany <2021170936@cis.asu.edu.eg>
2026-08-19 16:22:21 +0300

    Fix React crash caused by undefined bgmRef

--- src/pages/Overworld.jsx
+++ src/pages/Overworld.jsx
@@ -110,8 +110,6 @@ export default function Overworld() {
 
   return (
     <div className="fixed inset-0 w-screen h-[100dvh] overflow-hidden bg-retro-dark select-none touch-none">
-      {/* Audio Manager */}
-      <audio ref={bgmRef} src="/audio/overworld-bgm.mp3" loop />
       <MapCanvas 
         onNodeSelect={handleNodeSelect} 
         discovered={discovered} 
